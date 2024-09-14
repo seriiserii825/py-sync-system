fdacd72045b1809c05b1fb1ddeb69b31a40d8b6a
serii burduja <seriiburduja@gmail.com>
2024-09-14 08:09:48 +0300

    feat: refactor

--- main.py
+++ main.py
@@ -1,64 +1,23 @@
 import os
-from git import Repo
-from rich import print
-file_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'repos.txt')
-os.system(f"rm {file_path}")
-def reposToFile(file_path):
-    print('[green]Finding git repos')
-    command = f"find ~ -maxdepth 5 -name \".git\" -type d  > {file_path}"
-    os.system(command)
-    os.system(f"sed -i '/cache/d' {file_path}")
-    os.system(f"sed -i '/yay/d' {file_path}")
-    os.system(f"sed -i 's/.git//g' {file_path}")
-    os.system(f"bat {file_path}")
-def gitCommit():
-    print('[blue]Committing')
-    print('[green]1) Lazygit')
-    print('[blue]2) Commit')
-    choice = input('Enter your choice: ')
-    repo = Repo('.')
-    if choice == '1':
-        os.system('lazygit')
-        gitCommit()
-    elif choice == '2':
-        print('[green]Commit')
-        commit_message = input('Enter commit message: ')
-        repo.git.add(update=True)
-        repo.git.commit('-m', commit_message)
-        repo.git.push()
-        return
-    else:
-        print('Invalid Choice')
-        gitCommit()
-def gitPush(file_path):
-    print('[blue]Pushing')
-    with open(file_path, 'r') as f:
-        for line in f:
-            line = line.strip()
-            os.chdir(line)
-            repo = Repo('.')
-            if repo.is_dirty() or len(repo.untracked_files) > 0:
-                print(os.getcwd())
-                gitCommit()
-def gitPull(file_path):
-    print('[green]Pulling')
-    with open(file_path, 'r') as f:
-        for line in f:
-            line = line.strip()
-            os.chdir(line)
-            repo = Repo('.')
-            print(os.getcwd())
-            command = f"git remote update && git status -uno | grep -q 'Your branch is behind'"
-            result = os.system(command)
-            if result == 0:
-                print('Pulling')
-                repo.git.pull()
-    # rm = f"rm {file_path}"
-    # os.system(rm)
+from rich.console import Console
 
+from modules.gitPull import gitPull
+from modules.gitPush import gitPush
+from modules.reposToFile import reposToFile
+
+file_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'repos.txt')
+console = Console()
 
 def menu():
     reposToFile(file_path)
+    action = console.input("[green]Push[/] or [blue]Pull[/]? (push/pull): ")
+    if action == "push":
+        gitPush(file_path)
+    elif action == "pull":
+        gitPull(file_path)
+    else:
+        console.print("[red]Invalid option. Please try again.")
+        menu()
     gitPush(file_path)
     gitPull(file_path)
 menu()
