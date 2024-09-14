4553c26d344781426c7a851b6f6dbd9f8ca9065f
serii burduja <seriiburduja@gmail.com>
2024-09-14 09:48:31 +0300

    upd: push pull update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from rich.console import Console
 
+from libs.richTable import richTable
 from modules.gitPull import gitPull
 from modules.gitPush import gitPush
 from modules.reposToFile import reposToFile
@@ -10,9 +11,17 @@ console = Console()
 
 def menu():
     reposToFile(file_path)
-    action = console.input("[green]Push[/] or [blue]Pull[/]? (push/pull): ")
+    table_title = "Git Repository Manager"
+    table_columns = ["Option", "Description"]
+    table_rows = [
+        ["[blue]push[/]", "Push changes to the remote repository."],
+        ["[red]pull[/]", "Pull changes from the remote repository."]
+    ]
+    richTable(table_title, table_columns, table_rows)
+    action = console.input("[cyan]What would you like to do? ")
     if action == "push":
         gitPush(file_path)
+        menu()
     elif action == "pull":
         gitPull(file_path)
     else:
