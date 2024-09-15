3a35c1877283ffd2aace4f442b6e74fe355a33d0
serii burduja <seriiburduja@gmail.com>
2024-09-15 09:19:58 +0300

    upd: py sync git push delete submodule

--- modules/gitPush.py
+++ modules/gitPush.py
@@ -9,6 +9,7 @@ def gitPush(file_path):
     with open(file_path, 'r') as f:
         for line in f:
             line = line.strip()
+            print(f'Pushing {line}')
             os.chdir(line)
             repo = Repo('.')
             if repo.is_dirty() or len(repo.untracked_files) > 0:
