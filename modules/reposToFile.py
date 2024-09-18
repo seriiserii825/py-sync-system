34783cfc50e96fbb08cb5976367a800d29766b7d
serii burduja <seriiburduja@gmail.com>
2024-09-18 18:33:18 +0300

    upd: level 8

--- modules/reposToFile.py
+++ modules/reposToFile.py
@@ -2,10 +2,10 @@ import os
 
 def reposToFile(file_path):
     print('[green]Finding git repos')
-    command = f"find ~ -maxdepth 5 -name \".git\" -type d  > {file_path}"
+    command = f"find ~ -maxdepth 8 -name \".git\" -type d  > {file_path}"
     os.system(command)
     os.system(f"sed -i '/cache/d' {file_path}")
     os.system(f"sed -i '/yay/d' {file_path}")
     os.system(f"sed -i 's/.git//g' {file_path}")
-    os.system(f"bat {file_path}")
+    # os.system(f"bat {file_path}")
 
