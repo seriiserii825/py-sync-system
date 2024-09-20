d9850b4bb1d97c50c1c8e8c70dde23ae94949d5f
serii burduja <seriiburduja@gmail.com>
2024-09-20 09:59:56 +0300

    feat: repos files trash

--- modules/reposToFile.py
+++ modules/reposToFile.py
@@ -10,6 +10,7 @@ def reposToFile(file_path):
     os.system(f"sed -i '/autoload/d' {file_path}")
     os.system(f"sed -i '/oh-my-zsh/d' {file_path}")
     os.system(f"sed -i '/powerlevel10k/d' {file_path}")
+    os.system(f"sed -i '/Trash/d' {file_path}")
     os.system(f"sed -i '/advanced-custom-fields-wpcli/d' {file_path}")
     os.system(f"sed -i 's/.git//g' {file_path}")
     # os.system(f"bat {file_path}")
