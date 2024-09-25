950da73241abf2b16b33d65181e3848bc0ed5992
serii burduja <seriiburduja@gmail.com>
2024-09-25 09:01:34 +0300

    feat: commits to clipboard

--- modules/gitCommitToBuffer.py
+++ modules/gitCommitToBuffer.py
@@ -1,26 +1,25 @@
-import subprocess
-# from plyer import notification
-def gitCommitToBuffer():
-    # log=$(git log --since="3am" --pretty=tformat:"%s" --reverse > log.log);
-    # sed -i 's/feat://' log.log
-    # cat log.log | xclip -selection clipboard
-    # rm log.log
-    # bash code to python
-    command = [
-            "git", "log",
-            "--since=3am", "--pretty=tformat:%s", "--reverse"
-            ]
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.stdout:
-        result.stdout = result.stdout.replace('feat:', '')
-        command = [
-                "xclip", "-selection", "clipboard"
-                ]
-        result = subprocess.run(command, input=result.stdout, text=True)
-        print("[blue]Commits copied to clipboard.")
-        # notification.notify(
-        #         title="Commits copied to clipboard.",
-        #         message=result.stdout,
-        #         timeout=10
-        #         )
+import git
+from datetime import datetime
 
+from libs.buffer import addToClipBoard
+def gitCommitToBuffer():
+# Open the existing git repository
+    repo = git.Repo('.')
+
+# Define the time (you mentioned 3am, so we'll assume today at 3am)
+    since_time = datetime.now().replace(hour=3, minute=0, second=0, microsecond=0)
+
+# Get the git log messages since 3am, in reverse order
+    commits = repo.iter_commits(since=since_time, reverse=True)
+
+# Collect commit messages
+    commit_messages = [commit.message.strip() for commit in commits]
+    #remove feat: from commit_messages
+    clear_messages = []
+    for message in commit_messages:
+        clear_messages.append(message.replace('feat:',''))
+
+# Print the commit messages
+    print("Commit messages since 3am:")
+    message_text = '\n'.join(clear_messages)
+    addToClipBoard(message_text)
