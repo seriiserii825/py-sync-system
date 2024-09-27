83803d0834eed66a02d8969bf0ae09497e0d6c35
serii burduja <seriiburduja@gmail.com>
2024-09-27 11:18:04 +0300

    feat: pull pipInstall removed

--- modules/gitPull.py
+++ modules/gitPull.py
@@ -4,16 +4,6 @@ from modules.checkForGitDir import checkForGitDir
 from modules.checkIfPullNeeded import checkIfPullNeeded
 from rich.panel import Panel
 
-def pipInstall():
-    if not os.path.exists('venv'):
-        os.system('python3 -m venv venv')
-        os.system('source venv/bin/activate')
-        os.system('python3 -m pip install --upgrade pip')
-        os.system('python3 -m pip install -r requirements.txt')
-    else:
-        os.system('source venv/bin/activate')
-        os.system('python3 -m pip install -r requirements.txt')
-
 def gitModules():
     os.system('git submodule init')
     os.system('git submodule update')
@@ -35,14 +25,10 @@ def gitModules():
 def gitPull():
     print(Panel(f"Pulling from {os.getcwd()}", title="Git Pull", style="blue"))
     if checkForGitDir():
-        # check for gitmodules
         if os.path.exists('.gitmodules'):
             gitModules()
         result = checkIfPullNeeded()
         if result:
             os.system('git pull')
-            # check for python files
-            if os.path.exists('requirements.txt'):
-                pipInstall()
 
 
