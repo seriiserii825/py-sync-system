4fa4ec529d8485f898474e18dc02522376e0ad05
serii burduja <seriiburduja@gmail.com>
2024-10-03 11:21:17 +0300

    feat: on push if have gpgrc push

--- modules/checkIfPushNeeded.py
+++ modules/checkIfPushNeeded.py
@@ -1,8 +1,12 @@
+import os
 import git
 
 def checkIfPushNeeded():
+    # if exists .gpgrc and it's not empty
     my_repo = git.Repo('.')
-    if my_repo.is_dirty(untracked_files=True):
+    if os.path.exists('.gpgrc') and os.path.getsize('.gpgrc') > 0:
+        return True
+    elif my_repo.is_dirty(untracked_files=True):
         return True
     else:
         return False
