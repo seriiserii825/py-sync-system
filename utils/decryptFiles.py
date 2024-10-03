35ea2a2ef374333ea417eb6942b74f2b4f6df9e4
serii burduja <seriiburduja@gmail.com>
2024-10-03 11:29:03 +0300

    feat: decrypt files

--- utils/decryptFiles.py
+++ utils/decryptFiles.py
@@ -3,12 +3,13 @@ from rich import print
 from rich.panel import Panel
 user = os.getlogin()
 def decryptFiles():
-    print(f'[green]Decrypting files')
-    with open('.gpgrc', 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            line = line.replace('\n', '')
-            print(Panel(f'[blue]decrypt line: {line}'))
-            file_without_gpg = line.replace('.gpg', '')
-            os.system(f'rm {file_without_gpg}')
-            os.system(f'gpg -d {line} > {file_without_gpg}')
+    if os.path.isfile('.gpgrc'):
+        print(f'[green]Decrypting files')
+        with open('.gpgrc', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                line = line.replace('\n', '')
+                print(Panel(f'[blue]decrypt line: {line}'))
+                file_without_gpg = line.replace('.gpg', '')
+                os.system(f'rm {file_without_gpg}')
+                os.system(f'gpg -d {line} > {file_without_gpg}')
