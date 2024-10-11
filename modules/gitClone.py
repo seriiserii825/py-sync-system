295bfbe5d13f6677e0ed8069f12915b5cfe1ce27
serii burduja <seriiburduja@gmail.com>
2024-10-11 12:39:32 +0300

    feat: removed pipInstall

--- modules/gitClone.py
+++ modules/gitClone.py
@@ -1,14 +1,5 @@
 import subprocess
 import os
-import sys
-
-def pip_install():
-    if not os.path.isdir("venv"):
-        subprocess.run([sys.executable, "-m", "venv", "venv"], check=True)
-        activate_venv = "source venv/bin/activate"
-        subprocess.run(activate_venv, shell=True, check=True)
-        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"], check=True)
-        subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
 
 def gitClone():
     clipboard = subprocess.check_output("xclip -o -selection clipboard", shell=True, text=True).strip()
@@ -32,11 +23,6 @@ def gitClone():
                 "git submodule foreach 'branch=$(git branch -r | grep -m1 origin/HEAD | sed \"s/.*origin\\///\") && git checkout $branch && git pull origin $branch'",
                 shell=True, check=True
             )
-
-        # Check for requirements.txt and run pip install if it exists
-        if os.path.isfile("requirements.txt"):
-            pip_install()
-
     else:
         print("Clipboard does not contain a Git repository URL.")
 
